d3921f2f7abf0a1a774e1f2bfcecf873ac5fbdba
Ricardo Siu <ricardo.siu@quantum.com.hk>
2025-11-15 23:40:43 +0800

    Reduce answers to 3 sentences to be more concise.
    Updated answers to 3 sentences in code/chatui/prompts/prompts_llama3.py

--- code/chatui/prompts/prompts_llama3.py
+++ code/chatui/prompts/prompts_llama3.py
@@ -41,7 +41,7 @@ Here is the user question: {question}
 
 generator_prompt = """
 <|begin_of_text|><|start_header_id|>system<|end_header_id|> 
-You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use five sentences maximum and keep the answer concise but helpful. 
+You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise but helpful. 
 
 <|eot_id|><|start_header_id|>user<|end_header_id|>
 
